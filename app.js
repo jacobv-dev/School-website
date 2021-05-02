2114da93f998a27e7e21d52bcc5a1ef1a8e13d19
Jakub Vorel <kuba.vorel@email.cz>
2021-05-02 16:03:04 +0200

    BIG Update 👍🏻

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Galerie
+
 const menuopener = document.querySelector(".menu-opener");
 const menu = document.querySelector(".menu");
 const links = document.querySelectorAll(".menu li");
@@ -11,4 +13,19 @@ menuopener.addEventListener("click", () => {
         link.classList.toggle("fade")
     });
     
+});
+
+// Scrollování textu na main page
+
+$(document).ready(function(){
+    $(".slider").slick({
+    vertical: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 350,
+    arrows: false,
+    swipe: false,
+    infinite: true,
+    pauseOnHover: false,
+    });
 });
