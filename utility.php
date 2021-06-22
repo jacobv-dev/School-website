e5323ab95a2269a83ae1c608b4b5462ed1c8a1a3
jacobv-dev <kuba.vorel@email.cz>
2021-06-22 20:03:28 +0200

    Album cover

--- utility.php
+++ utility.php
@@ -79,9 +79,12 @@
                                 
                 $autor = $json_array['current']['interpret'];
                 $song = $json_array['current']['song'];
+                $cover = $json_array['current']['image'];
                 $zacatek = substr($json_array['current']['begin'], 10);
                 $konec = substr($json_array['current']['end'], 10);
 
+                echo "<a style='cursor: default;' href=\"$cover\" target='_blank'>" . "<img style=\"height: 200px; width: 200px; margin: 25px; cursor: pointer; margin: 0 0 25px 0;\" src=" . $cover . " ></a>";
+                echo "<br>";
                 echo $autor . " - " . $song; // Vypsání Autora a Skladby
                 echo "<br>" . "<br>";
                 echo '<p style="font-size: 0.9em; letter-spacing: 0.05em;">' . $zacatek . " - " . $konec . '</p>'; // Vypsání Začátku a Konce skladby
@@ -170,6 +173,8 @@
 	        </fieldset>
 	
         </div>
+
+        <p style="margin-bottom: 20px; cursor: default;">API Hitrádia Zlín převzato z webu Radia.cz</p>
         </section>
         
     <script type="text/javascript" src="app.js"></script>
