d22f1c709da5df241262507e607c59a93e4365b3
jacobv-dev <kuba.vorel@email.cz>
2022-01-03 21:07:09 +0100

    Animace padák

---
+++ animace/ctverec.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+  <?php error_reporting(0); ?>
+<html>
+
+<!-- POHYBUJÍCÍ SE ČTVEREC -->
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+  <meta name="keywords" content="animace" lang="cs" />
+  <meta name="author" content="Jakub Vorel" />
+  <title>Animovaný čtverec</title>
+
+  <style type="text/css">
+    body {
+      background: #222;
+      font-family: "Open Sans", sans-serif;
+    }
+
+    .go {
+      animation-name: example;
+      animation-duration: 6s;
+      animation-iteration-count: infinite;
+      animation-direction: alternate;
+      animation-timing-function: ease-in;
+    }
+
+    @keyframes example {
+      from {
+        transform: rotate(0deg);
+      }
+
+      to {
+        transform: rotate(150deg);
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <div class="cely_web">
+
+    <?php
+    for ($i = 1; $i <= 80; $i++)
+      echo "<div class=\"go\" style=\"
+              position: absolute;
+              top: 30%;
+              left: 35%;
+              background: rgba(244,164,9,0.02);
+              border: 2px solid rgba(244,164,9,0.025); 
+              width: ", $i * 5 + 100, "px;
+              height: ", $i * 5 + 100, "px;
+              transform: rotate(", $i * 10, "deg);
+              animation-delay: ", $i * 0.10, "s;\">
+              </div>";
+    ?>
+
+    <div style="color: white; position: fixed; bottom: 5px; right: 5px; ">Jakub Vorel, 2021</div>
+    <div style="color: white; position: fixed; left: 5px; bottom: 5px; ">PHP + HTML + CSS + JS</div>
+  </div>
+
+  </div>
+</body>
+
+</html>
